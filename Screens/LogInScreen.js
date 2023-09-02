cb4b3f06e3622be12ba2ff7c508d9cfe2392d9ea
Mohit Kumavat <kumavatmohit007@gmail.com>
2023-09-01 21:39:46 -0700

    loginScreen demo

---
+++ Screens/LogInScreen.js
@@ -0,0 +1,15 @@
+import {SafeAreaView, StyleSheet, View, Text } from "react-native";
+import React from 'react'
+import { StyleContainer, InnerContainer, PageLogo, PageTitle } from "../components/styles";
+
+export default function LoginScreen() {
+    return(
+       <StyleContainer>
+        <InnerContainer>
+            <PageTitle>
+              VISION by Aabhas
+            </PageTitle>
+        </InnerContainer>
+       </StyleContainer>
+    );
+}
